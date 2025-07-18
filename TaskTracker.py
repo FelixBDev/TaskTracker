87db68133d6643a30249d2a844d361daae76b266
Kazou725 <felix.bourassa.2004@hotmail.com>
2025-07-17 21:16:22 -0400

    Update and remove function
    User can add, update and remove task

--- TaskTracker.py
+++ TaskTracker.py
@@ -3,19 +3,19 @@ class Task:
     def __init__(self, name, status):
         self.name = name
         self.status = status
-
+    
     def __str__(self):
-        return f"Task : {self.name}, status : {self.status})"
+        return f"Task : {self.name} || Status : {self.status}"
 
-allTask = [Task]
+allTask = []
 numOfTask = 0
 
 #Information sur comment marche l'application
 print('What do you want to do?')
-print('If you want to add a task write : new task')
-print('If you want to update a task write : update task')
-print('If you want to remove a task write : remove task')
-print('If you want to see your list simply write : to do list')
+print('If you want to add a task write : add')
+print('If you want to update a task write : update')
+print('If you want to remove a task write : remove')
+print('If you want to see your list simply write : list')
 print('If you want to exit simply write : exit')
 
 
@@ -24,29 +24,35 @@ print('If you want to exit simply write : exit')
 while True:
     try:
         userAction = input('> ')
-        if userAction == 'new task':
+        if userAction == 'add':
             print('Please write the name of the task.')
             
             taskName = input('> ')
-            globals()[f"task{numOfTask}"] = Task(taskName, 'To do')
             allTask.append(Task(taskName, 'To do'))
-            numOfTask = numOfTask + 1
 
 
-        elif userAction == 'remove task':
+        elif userAction == 'remove':
+
             print('Please write the name of the task you want to remove')
-            allTask.remove(input('> '))
-            print('Your current task are ' + str(allTask))
 
-        elif userAction == 'update task':
+            taskToRemove = input('> ')
+            for i in allTask:
+                if i.name == taskToRemove:
+                    allTask.remove(i)
+                    print('Task is removed.')
+                else:
+                    print('Task not found')
+
+        elif userAction == 'update':
             print('Please write the name of the task you want to update')
-            allTask.remove(input('> '))
-            print('What do you want it to say now?')
-            allTask.append(input('> '))
+            taskToUpdate = input('> ')
+            for i in allTask:
+                if i.name == taskToUpdate:
+                    print('Changing the task ' + i.name)
+                    print('Write the new status.')
+                    i.status = input('> ')
 
-            print('Your current task are ' + str(allTask))
-
-        elif userAction == 'to do list':
+        elif userAction == 'list':
             for i in allTask:
                 print(i)
         
